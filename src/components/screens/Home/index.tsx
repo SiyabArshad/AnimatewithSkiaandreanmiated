f9f92ec91978ad347e48e60ee1458121208781be
SiyabArshad <siyabarshadsatti@gmail.com>
2023-12-26 22:06:11 +0500

    All animation list down

--- src/components/screens/Home/index.tsx
+++ src/components/screens/Home/index.tsx
@@ -20,15 +20,177 @@ export default function Home({navigation}) {
     },
     {
       id: 2,
-      name: 'Bending Circle',
+      name: 'Chassing Bubbles',
+      using: 'Skia and Reanimated',
+      onPress: () => {
+        navigation?.navigate(routes.clockSkia);
+      },
+    },
+
+    {
+      id: 3,
+      name: 'Restrict box around a circle',
+      using: 'Pan-Gesture-Handler and Reanimated',
+      onPress: () => {
+        navigation?.navigate(routes.clockSkia);
+      },
+    },
+
+    {
+      id: 4,
+      name: 'Animated Onboarding animation',
+      using: 'Pan-Gesture-Handler and Reanimated',
+      onPress: () => {
+        navigation?.navigate(routes.clockSkia);
+      },
+    },
+
+    {
+      id: 5,
+      name: 'Dark mode using interpolate',
+      using: 'Reanimated',
+      onPress: () => {
+        navigation?.navigate(routes.clockSkia);
+      },
+    },
+
+    {
+      id: 6,
+      name: 'Zoom in and out',
+      using: 'Pinch-Gesture-Handler and Reanimated',
+      onPress: () => {
+        navigation?.navigate(routes.clockSkia);
+      },
+    },
+
+    {
+      id: 7,
+      name: 'Instagram Like Animation',
+      using: 'Reanimated',
+      onPress: () => {
+        navigation?.navigate(routes.clockSkia);
+      },
+    },
+
+    {
+      id: 8,
+      name: 'Color Picker Animation',
+      using: 'Gesture Handler and Reanimated',
+      onPress: () => {
+        navigation?.navigate(routes.clockSkia);
+      },
+    },
+
+    {
+      id: 9,
+      name: 'Animated Circular Bar',
+      using: 'Reanimated',
+      onPress: () => {
+        navigation?.navigate(routes.clockSkia);
+      },
+    },
+
+    {
+      id: 10,
+      name: 'Swipe to Delete Animation',
+      using: 'Gesture-Handler and Reanimated',
+      onPress: () => {
+        navigation?.navigate(routes.clockSkia);
+      },
+    },
+
+    {
+      id: 11,
+      name: 'Ripple Effect Animation',
+      using: 'Reanimated',
+      onPress: () => {
+        navigation?.navigate(routes.clockSkia);
+      },
+    },
+
+    {
+      id: 12,
+      name: 'Clock Animation',
+      using: 'Reanimated',
+      onPress: () => {
+        navigation?.navigate(routes.clockSkia);
+      },
+    },
+
+    {
+      id: 13,
+      name: 'Perspective Menu Animation',
+      using: 'Reanimated',
+      onPress: () => {
+        navigation?.navigate(routes.clockSkia);
+      },
+    },
+
+    {
+      id: 14,
+      name: 'Layout Animations',
+      using: 'Reanimated',
+      onPress: () => {
+        navigation?.navigate(routes.clockSkia);
+      },
+    },
+
+    {
+      id: 15,
+      name: 'Flatlist animation',
+      using: 'Reanimated',
+      onPress: () => {
+        navigation?.navigate(routes.clockSkia);
+      },
+    },
+
+    {
+      id: 16,
+      name: 'DropDown Animation',
+      using: 'Reanimated',
+      onPress: () => {
+        navigation?.navigate(routes.clockSkia);
+      },
+    },
+
+    {
+      id: 17,
+      name: 'Skelton Loading Animation',
+      using: 'Reanimated',
+      onPress: () => {
+        navigation?.navigate(routes.clockSkia);
+      },
+    },
+
+    {
+      id: 18,
+      name: 'D3 Skia charts ',
+      using: 'Skia and Reanimated',
+      onPress: () => {
+        navigation?.navigate(routes.clockSkia);
+      },
+    },
+
+    {
+      id: 19,
+      name: 'Donut Progress circle using Skia',
+      using: 'Skia and Reanimated',
+      onPress: () => {
+        navigation?.navigate(routes.clockSkia);
+      },
+    },
+
+    {
+      id: 20,
+      name: 'Morphing Circle Animation using Skia',
       using: 'Skia and Reanimated',
       onPress: () => {
         navigation?.navigate(routes.clockSkia);
       },
     },
     {
-      id: 3,
-      name: 'Chassing Bubbles',
+      id: 21,
+      name: 'Focus Animation using Skia',
       using: 'Skia and Reanimated',
       onPress: () => {
         navigation?.navigate(routes.clockSkia);
